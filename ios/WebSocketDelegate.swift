e35aa419b684f56d0f9ba2f49c8faf81fcb4c2cf
Lyle Pratt <lylepratt@gmail.com>
2015-09-18 16:49:08 -0500

    Update for iOS 9 Swift issues.

--- ios/WebSocketDelegate.swift
+++ ios/WebSocketDelegate.swift
@@ -12,26 +12,31 @@ class WebSocketDelegate : NSObject, SRWebSocketDelegate {
     
     func dispatch(json: AnyObject) {
         var error: NSError?
-        let data = NSJSONSerialization.dataWithJSONObject(json,
-            options: NSJSONWritingOptions.allZeros,
-            error: &error)
+        let data: NSData?
+        do {
+            data = try NSJSONSerialization.dataWithJSONObject(json,
+                        options: NSJSONWritingOptions())
+        } catch let error1 as NSError {
+            error = error1
+            data = nil
+        }
         if let message = data {
             self.plugin.dispatch(self.callbackId, message: message)
         } else {
             if let jsonError = error {
-                println("ERROR: \(jsonError.localizedDescription)")
+                print("ERROR: \(jsonError.localizedDescription)")
             }
         }
     }
     
     func webSocketDidOpen(webSocket: SRWebSocket!) {
-        println("INFO: A Web Socket has been opened.")
+        print("INFO: A Web Socket has been opened.")
         let json: AnyObject = ["name": "onopen"]
         dispatch(json)
     }
     
     func webSocket(websocket: SRWebSocket!, didFailWithError error: NSError!) {
-        println("INFO: A Web Socket has failed with an error.")
+        print("INFO: A Web Socket has failed with an error.")
         let json: AnyObject = [
             "name": "onerror",
             "parameters": [error.localizedDescription]
@@ -40,8 +45,8 @@ class WebSocketDelegate : NSObject, SRWebSocketDelegate {
     }
     
     func webSocket(webSocket: SRWebSocket!, didCloseWithCode code: NSInteger!,
-                   reason: NSString!, wasClean: Boolean!) {
-        println("INFO: A Web Socket has been closed with code \"\(code)\" and reason \"\(reason)\".")
+                   reason: NSString!, wasClean: DarwinBoolean!) {
+        print("INFO: A Web Socket has been closed with code \"\(code)\" and reason \"\(reason)\".")
         var object: AnyObject
         if(wasClean != nil) {
             object = ["code": code, "reason": reason, "wasClean": true]
@@ -56,7 +61,7 @@ class WebSocketDelegate : NSObject, SRWebSocketDelegate {
     }
     
     func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject!) {
-        println("INFO: Received a message: \(message)")
+        print("INFO: Received a message: \(message)")
         let object: AnyObject = ["data": message]
         let json: AnyObject = [
             "name": "onmessage",
